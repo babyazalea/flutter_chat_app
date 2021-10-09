3cc729ce542979f5234b3a392e7257e5d780d597
babyazalea <usun12@hotmail.com>
2021-10-09 11:01:52 +0900

    fetching & adding data with StreamBuilder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,31 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text("This works!"),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/7NPATrdJJwjVDKWJib7z/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
           Firestore.instance
               .collection('chats/7NPATrdJJwjVDKWJib7z/messages')
-              .snapshots()
-              .listen(
-            (data) {
-              data.documents.forEach((document) {
-                print(document['text']);
-              });
-            },
-          );
+              .add({
+            'text': 'This was added by clicking button!',
+          });
         },
       ),
     );
