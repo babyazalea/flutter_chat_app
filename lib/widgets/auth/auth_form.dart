52e120c65052d076824e1bbb0c6bac4c32928343
babyazalea <usun12@hotmail.com>
2021-10-11 14:19:24 +0900

    added firebase auth

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
+  AuthForm(this.submitFn);
+
+  final void Function(
+    String email,
+    String password,
+    String username,
+    bool isLogin,
+    BuildContext ctx,
+  ) submitFn;
+
   @override
   _AuthFormState createState() => _AuthFormState();
 }
@@ -18,9 +28,14 @@ class _AuthFormState extends State<AuthForm> {
 
     if (isValid) {
       _formKey.currentState.save();
-      print(_userEmail);
-      print(_userName);
-      print(_userPassword);
+
+      widget.submitFn(
+        _userEmail.trim(),
+        _userPassword,
+        _userName,
+        _isLogin,
+        context,
+      );
     }
   }
 
