853e38974f9acaeb3aac9579d7dafebff3678ae7
babyazalea <usun12@hotmail.com>
2021-10-13 16:26:12 +0900

    changing screen by authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_app/screens/auth_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import './screens/auth_screen.dart';
 import './screens/chat_screen.dart';
 
 void main() {
@@ -25,7 +27,15 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.hasData) {
+            return ChatScreen();
+          }
+          return AuthScreen();
+        },
+      ),
     );
   }
 }
