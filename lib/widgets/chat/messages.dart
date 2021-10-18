a691c8e679c05c3971337018ae324c02c079c802
babyazalea <usun12@hotmail.com>
2021-10-19 00:13:01 +0900

    wrapped message by bubble

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:flutter_chat_app/widgets/chat/message_bubble.dart';
+
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: Firestore.instance.collection('chat').orderBy('createdAt', descending: true).snapshots(),
+      stream: Firestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
       builder: (ctx, chatSnapshot) {
         if (chatSnapshot.connectionState == ConnectionState.waiting) {
           return Center(
@@ -16,9 +21,7 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocs.length,
-          itemBuilder: (ctx, index) => Text(
-            chatDocs[index]['text'],
-          ),
+          itemBuilder: (ctx, index) => MessageBubble(chatDocs[index]['text']),
         );
       },
     );
